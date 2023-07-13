7ca8e7848e7e999a2c6d3a23b81baf25ec7cc826
s22008 <s22008@std.it-college.ac.jp>
2023-07-13 15:52:36 +0900

    4.2.8

--- HelloSample/app/src/main/java/jp/ac/it_college/std/s22008/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/jp/ac/it_college/std/s22008/hellosample/MainActivity.kt
@@ -2,10 +2,43 @@ package jp.ac.it_college.std.s22008.hellosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ボタン
+        val btClick = findViewById<Button>(R.id.btClick)
+
+        //リスナクラスにインタンス
+        val listener = HelloLIstner()
+
+        //ボタンにリスナ
+        btClick.setOnClickListener(listener)
+    }
+
+    private inner class HelloLIstner: View.OnClickListener {
+        override fun onClick(v: View?) {
+            //名前記入欄
+            val input = findViewById<EditText>(R.id.etName)
+
+            //メッセージのオブジェクト
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            //名前文字列
+            val inputStr = input.text.toString()
+
+            //メッセージ表示
+            output.text = inputStr + "さん、こんにちは!"
+
+
+
+        }
+
     }
 }
