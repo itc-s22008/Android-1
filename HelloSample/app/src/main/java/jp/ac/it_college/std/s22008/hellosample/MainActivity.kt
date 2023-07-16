53ffc0520a77dda9fc8e5e370c26658942dda85f
s22008 <s22008@std.it-college.ac.jp>
2023-07-16 18:25:26 +0900

    3章終了

--- HelloSample/app/src/main/java/jp/ac/it_college/std/s22008/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/jp/ac/it_college/std/s22008/hellosample/MainActivity.kt
@@ -6,35 +6,50 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import jp.ac.it_college.std.s22008.hellosample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //を使い画面作成
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Binding クラスインタンス生成
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //管理されたものを使う
+        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
         //ボタン
-        val btClick = findViewById<Button>(R.id.btClick)
+        //val btClick = findViewById<Button>(R.id.btClick)
 
         //リスナクラスにインタンス
         val listener = HelloLIstner()
 
         //ボタンにリスナ
-        btClick.setOnClickListener(listener)
+        binding.btClick.setOnClickListener(listener)
     }
 
     private inner class HelloLIstner: View.OnClickListener {
         override fun onClick(v: View?) {
             //名前記入欄
-            val input = findViewById<EditText>(R.id.etName)
+            //val input = findViewById<EditText>(R.id.etName)
+            val input = binding.etName
 
             //メッセージのオブジェクト
-            val output = findViewById<TextView>(R.id.tvOutput)
+            val output = binding.tvOutput
 
             //名前文字列
-            val inputStr = input.text.toString()
+            //val inputStr = input.text.toString()
+            val inputStr = binding.etName.text.toString()
 
             //メッセージ表示
-            output.text = inputStr + "さん、こんにちは!"
+            //output.text = inputStr + "さん、こんにちは!"
+            binding.tvOutput.text = "${inputStr}さん、こんにちは"
 
 
 
