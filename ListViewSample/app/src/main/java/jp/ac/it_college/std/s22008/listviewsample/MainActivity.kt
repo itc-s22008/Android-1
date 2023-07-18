7dfd84e8c3a366e2cbf3659a128358edc29e63fd
s22008 <s22008@std.it-college.ac.jp>
2023-07-18 15:59:34 +0900

    5.1.5まで完了

--- ListViewSample/app/src/main/java/jp/ac/it_college/std/s22008/listviewsample/MainActivity.kt
+++ ListViewSample/app/src/main/java/jp/ac/it_college/std/s22008/listviewsample/MainActivity.kt
@@ -2,10 +2,29 @@ package jp.ac.it_college.std.s22008.listviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+import jp.ac.it_college.std.s22008.listviewsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val item = parent?.getItemAtPosition(position) as String
+            val show = "あなたが選んだ定食: $item"
+            val lvMenu = findViewById<ListView>(R.id.lvMenu)
+            lvMenu.onItemClickListener = ListItemClickListener()
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG).show()
+
+        }
     }
 }
